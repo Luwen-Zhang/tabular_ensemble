8d9cc0a23f233cc28d4d560df2d81b7284f6dfdc
LuoXueling <sharrin@sjtu.edu.cn>
2023-07-28 18:18:43 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,13 +24,13 @@ setup(
         "torch": ["torch>=1.12.0"],
         "all": req_all,
         "test": [
-	    "torch>=1.12.0",
+            "torch>=1.12.0",
             "pytest",
             "pytest-cov",
             "pytest-order",
             "pytest-mock",
             "black",
- 	],
+        ],
         "doc": [
             "sphinx",
             "sphinx_rtd_theme",
