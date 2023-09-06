0531166c9e2f063cb14c048f532449e8b07bedd4
LuoXueling <sharrin@sjtu.edu.cn>
2023-09-06 13:09:36 +0800

    Suppress invalid escape sequence.

--- tabensemb/utils/utils.py
+++ tabensemb/utils/utils.py
@@ -458,7 +458,11 @@ def str_to_dataframe(s, sep=",", names=None, check_nan_on=None) -> pd.DataFrame:
     pd.DataFrame
         The converted dataframe.
     """
-    df = pd.read_csv(StringIO(s), names=names, sep=sep)
+    with warnings.catch_warnings():
+        warnings.filterwarnings(
+            "ignore", "invalid escape sequence", category=DeprecationWarning
+        )
+        df = pd.read_csv(StringIO(s), names=names, sep=sep)
     if names is not None:
         if len(df.columns) != len(names) or (
             df.dtypes[names[0]] == object and pd.isna(df[names[1:]]).all().all()
@@ -693,7 +697,11 @@ def plot_pdp(
         right=False,
     )
     plt.ylabel("Predicted target")
-    plt.xlabel("Value of predictors ($10\%$-$90\%$ percentile)")
+    with warnings.catch_warnings():
+        warnings.filterwarnings(
+            "ignore", "invalid escape sequence", category=DeprecationWarning
+        )
+        plt.xlabel("Value of predictors ($10\%$-$90\%$ percentile)")
 
     return fig
 
